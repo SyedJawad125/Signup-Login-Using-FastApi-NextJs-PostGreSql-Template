ee8da4d71a778d9bdc9587659b84352706939ad2
Syed Jawad Ali <syedjawadali92@gmail.com>
2025-08-14 11:36:18 +0500

    Changes

--- backend/add_permissions.py
+++ backend/add_permissions.py
@@ -8,50 +8,10 @@ permissions = [
     {"name": "Update Role", "code": "update_role", "module_name": "Role", "description": "User can update role"},
     {"name": "Delete Role", "code": "delete_role", "module_name": "Role", "description": "User can delete role"},
     
-    {"name": "Create Department", "code": "create_department", "module_name": "Department", "description": "User can create Department"},
-    {"name": "Read Department", "code": "read_department", "module_name": "Department", "description": "User can read Department"},
-    {"name": "Update Department", "code": "update_department", "module_name": "Department", "description": "User can update Department"},
-    {"name": "Delete Department", "code": "delete_department", "module_name": "Department", "description": "User can delete Department"},
-
     {"name": "Create Employee", "code": "create_employee", "module_name": "Employee", "description": "User can create Employee"},
     {"name": "Read Employee", "code": "read_employee", "module_name": "Employee", "description": "User can read Employee"},
     {"name": "Update Employee", "code": "update_employee", "module_name": "Employee", "description": "User can update Employee"},
     {"name": "Delete Employee", "code": "delete_employee", "module_name": "Employee", "description": "User can delete Employee"},
-
-    {"name": "Create Rank", "code": "create_rank", "module_name": "Rank", "description": "User can create Rank"},
-    {"name": "Read Rank", "code": "read_rank", "module_name": "Rank", "description": "User can read Rank"},
-    {"name": "Update Rank", "code": "update_rank", "module_name": "Rank", "description": "User can update Rank"},
-    {"name": "Delete Rank", "code": "delete_rank", "module_name": "Rank", "description": "User can delete Rank"},
-
-    {"name": "Create Leave", "code": "create_leave", "module_name": "Leave", "description": "User can create Leave"},
-    {"name": "Read Leave", "code": "read_leave", "module_name": "Leave", "description": "User can read Leave"},
-    {"name": "Update Leave", "code": "update_leave", "module_name": "Leave", "description": "User can update Leave"},
-    {"name": "Delete Leave", "code": "delete_leave", "module_name": "Leave", "description": "User can delete Leave"},
-
-    {"name": "Create Notification", "code": "create_notification", "module_name": "Notification", "description": "User can create Notification"},
-    {"name": "Read Notification", "code": "read_notification", "module_name": "Notification", "description": "User can read Notification"},
-    {"name": "Update Notification", "code": "update_notification", "module_name": "Notification", "description": "User can update Notification"},
-    {"name": "Delete Notification", "code": "delete_notification", "module_name": "Notification", "description": "User can delete Notification"},
-
-    {"name": "Create Employee Salary", "code": "create_employee_salary", "module_name": "Employee Salary", "description": "User can create Employee Salary"},
-    {"name": "Read Employee Salary", "code": "read_employee_salary", "module_name": "Employee Salary", "description": "User can read Employee Salary"},
-    {"name": "Update Employee Salary", "code": "update_employee_salary", "module_name": "Employee Salary", "description": "User can update Employee Salary"},
-    {"name": "Delete Employee Salary", "code": "delete_employee_salary", "module_name": "Employee Salary", "description": "User can delete Employee Salary"},
-
-    {"name": "Create Salary Structure", "code": "create_salary_structure", "module_name": "Salary Structure", "description": "User can create Salary Structure"},
-    {"name": "Read Salary Structure", "code": "read_salary_structure", "module_name": "Salary Structure", "description": "User can read Salary Structure"},
-    {"name": "Update Salary Structure", "code": "update_salary_structure", "module_name": "Salary Structure", "description": "User can update Salary Structure"},
-    {"name": "Delete Salary Structure", "code": "delete_salary_structure", "module_name": "Salary Structure", "description": "User can delete Salary Structure"},
-
-    {"name": "Create Payslip", "code": "create_payslip", "module_name": "Payslip", "description": "User can create Payslip"},
-    {"name": "Read Payslip", "code": "read_payslip", "module_name": "Payslip", "description": "User can read Payslip"},
-    {"name": "Update Payslip", "code": "update_payslip", "module_name": "Payslip", "description": "User can update Payslip"},
-    {"name": "Delete Payslip", "code": "delete_payslip", "module_name": "Payslip", "description": "User can delete Payslip"},
-
-    {"name": "Create Salary History", "code": "create_salary_history", "module_name": "Salary History", "description": "User can create Salary History"},
-    {"name": "Read Salary History", "code": "read_salary_history", "module_name": "Salary History", "description": "User can read Salary History"},
-    {"name": "Update Salary History", "code": "update_salary_history", "module_name": "Salary History", "description": "User can update Salary History"},
-    {"name": "Delete Salary History", "code": "delete_salary_history", "module_name": "Salary History", "description": "User can delete Salary History"},
 
     {"name": "Create Image", "code": "create_image", "module_name": "Image", "description": "User can create Image"},
     {"name": "Read Image", "code": "read_image", "module_name": "Image", "description": "User can read Image"},
